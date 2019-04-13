4492cb535d96968ff6337449ba2ddfe89282933a
Chase Kelly <chaserkelly3211@gmail.com>
2019-04-13 12:05:45 -0700

    Ajax get/post requests

--- server.js
+++ server.js
@@ -21,15 +21,15 @@ var customers = [
 
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, ""));
+    res.sendFile(path.join(__dirname, "home.html"));
   });
   
   app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, ""));
+    res.sendFile(path.join(__dirname, "reserve.html"));
   });
   
   app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, ""));
+    res.sendFile(path.join(__dirname, "tables.html"));
   });
 
   app.listen(PORT, function() {
